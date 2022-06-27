d1df8e9d8fa3375a482464077fe564246feafa47
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-06-27 18:39:29 +0300

    double

--- srcs/init_game/create_img_to_map.c
+++ srcs/init_game/create_img_to_map.c
@@ -36,15 +36,15 @@ void	check_len_ray(t_ray hor, t_ray ver, t_data *data)
 void	init_ver(t_ray *ver, t_pl pl)
 {
 	if (pl.dir < (float)M_PI_2 && pl.dir > (float)(M_PI * 1.5))
-		ver->x = (int)pl.x / SCALE * SCALE + 1;
+		ver->x = (int)pl.x / SCALE * SCALE + SCALE;
 	else
 		ver->x = (int)pl.x / SCALE * SCALE - 1;
-	ver->y = (int)pl.y + (tan(pl.dir) * fabsf(ver->x - pl.x)) - 1;
+	ver->y = (int)pl.y + (tanf(pl.dir) * fabsf(ver->x - pl.x));
 	ver->s_x = SCALE;
+	ver->s_y = tanf(pl.dir) * fabsf(ver->s_x);
 	if (pl.dir > (float)M_PI_2 && pl.dir < (float)(M_PI * 1.5))
 		ver->s_x *= -1;
-	ver->s_y = tan(pl.dir) * fabsf(ver->s_x - pl.x);
-	ver->len_ray = fabs((pl.x - ver->x) / cos(pl.dir));
+	ver->len_ray = fabs(sqrt(powf(pl.x - ver->x, 2.0) + powf(pl.y - ver->y, 2.0)));
 }
 
 void	init_hor(t_ray *hor, t_pl pl)
@@ -52,13 +52,13 @@ void	init_hor(t_ray *hor, t_pl pl)
 	if (pl.dir < (float)M_PI)
 		hor->y = (int)pl.y / SCALE * SCALE - 1;
 	else
-		hor->y = (int)pl.y / SCALE * SCALE + 1;
-	hor->x = ((int)pl.x + ((fabsf(pl.y - hor->y))) / tan(pl.dir));
+		hor->y = (int)pl.y / SCALE * SCALE + SCALE;
+	hor->x = (int)pl.x + (fabsf(pl.y - hor->y)) / tanf(pl.dir);
 	hor->s_y = SCALE;
 	if (pl.dir < (float)M_PI)
 		hor->s_y *= -1;
-	hor->s_x = fabsf(hor->s_y) / tan(pl.dir);
-	hor->len_ray = fabs((pl.x - hor->x) / cos(pl.dir));
+	hor->s_x = fabsf(hor->s_y) / tanf(pl.dir);
+	hor->len_ray = fabs(sqrt(powf(pl.x - hor->x, 2.0) + powf(pl.y - hor->y, 2.0)));
 }
 
 int	check_wall(t_ray *hv, char **map)
@@ -74,9 +74,9 @@ int	check_wall(t_ray *hv, char **map)
 	|| x > (int)ft_strlen(map[y]))
 	{
 		if (y < 0)
-			hv->y = SCALE;
+			hv->y = SCALE - 1;
 		if (y > len_map)
-			hv->y = len_map * SCALE;
+			hv->y = len_map * SCALE + SCALE;
 		if (x < 0)
 			hv->x = SCALE;
 		if (x > (int)ft_strlen(map[(int)hv->y / SCALE]))
@@ -115,17 +115,25 @@ void	draw_player(t_data *data)
 			ver.x += ver.s_x;
 			ver.y += ver.s_y;
 			wall_v = check_wall(&ver, data->map);
-			ver.len_ray = fabs((data->pl->x - ver.x) / cos(data->pl->dir));
+			ver.len_ray = fabs((data->pl->x - ver.x) / cosf(data->pl->dir));
 		}
 		if (!wall_h && ver.len_ray >= hor.len_ray)
 		{
 			hor.x += hor.s_x;
 			hor.y += hor.s_y;
 			wall_h = check_wall(&hor, data->map);
-			hor.len_ray = fabs((data->pl->x - hor.x) / cos(data->pl->dir));
+			hor.len_ray = fabs((data->pl->x - hor.x) / cosf(data->pl->dir));
 		}
+		if (hor.len_ray > ver.len_ray && wall_v == 1)
+			wall_h = 1;
+		if (ver.len_ray > hor.len_ray && wall_h == 1)
+			wall_v = 1;
+		printf("ver y=%d, x=%d, len=%f\n", (int)ver.y / SCALE, (int)ver.x / SCALE, ver.len_ray);
+		printf("hor y=%d, x=%d, len=%f\n\n\n", (int)hor.y / SCALE, (int)hor.x / SCALE, hor.len_ray);
+		my_mlx_pixel_put(data->mlx, ver.x, ver.y, 0x00FF000);
+		my_mlx_pixel_put(data->mlx, hor.x, hor.y, 0x00FF000);
 	}
-	check_len_ray(hor, ver, data);
+	// check_len_ray(hor, ver, data);
 }
 
 void	draw_map(t_data *data)
--- srcs/init_game/moves.c
+++ srcs/init_game/moves.c
@@ -5,8 +5,8 @@ void	move_left(t_data *data)
 	data->pl->dir += 0.1;
 	if (data->pl->dir >= 2 * (float)M_PI)
 		data->pl->dir -= (float)M_PI * 2;
-	data->pl->dx = cos(data->pl->dir);
-	data->pl->dy = sin(data->pl->dir);
+	data->pl->dx = cosf(data->pl->dir);
+	data->pl->dy = sinf(data->pl->dir);
 }
 
 void	move_right(t_data *data)
@@ -14,8 +14,8 @@ void	move_right(t_data *data)
 	data->pl->dir -= 0.1;
 	if (data->pl->dir < 0)
 		data->pl->dir += (float)M_PI * 2;
-	data->pl->dx = cos(data->pl->dir);
-	data->pl->dy = sin(data->pl->dir);
+	data->pl->dx = cosf(data->pl->dir);
+	data->pl->dy = sinf(data->pl->dir);
 }
 
 void	move_up(t_data *data)
