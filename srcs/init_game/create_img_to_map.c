b3bf1412e94c771533b1d795761df5325ecb871d
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-06-27 11:17:11 +0300

    bp

--- srcs/init_game/create_img_to_map.c
+++ srcs/init_game/create_img_to_map.c
@@ -25,110 +25,67 @@ void	draw_block(t_mlx *mlx, int x, int y, int color)
 	}
 }
 
-int	check_x_y_win(float *x, float *y)
+void	check_len_ray(t_ray hor, t_ray ver, t_data *data)
 {
-	if (*x < 0 || *y < 0 || *x > (float)WIN_X || *y > (float)WIN_Y)
-	{
-		if (*x < 0)
-			*x = 0;
-		if (*y < 0)
-			*y = 0;
-		if (*x > WIN_X)
-			*x = WIN_X;
-		if (*y > WIN_Y)
-			*y = WIN_Y;
-		return (1);
-	}
-	return (0);
+	if (hor.len_ray < ver.len_ray)
+		my_mlx_pixel_put(data->mlx, hor.x, hor.y, 0x00FF000);
+	else
+		my_mlx_pixel_put(data->mlx, ver.x, ver.y, 0x00FF000);
 }
-
-// void	horizontal_check(t_data	*data, int *wall)
-// {
-// 	float	a_x;
-// 	float	a_y;
-// 	t_ray	*ray;
-
-// 	ray = data->ray;
-// 	if (ray->hx == 0 && ray->hy == 0)
-// 	{
-// 		ray->hy = (int)(data->pl->y / SCALE) * SCALE;
-// 		ray->hx = (int)(data->pl->x / SCALE) * SCALE + ((fabsf(data->pl->y - ray->hy)) / tan(data->pl->dir));
-// 	}
-// 	a_y = SCALE;
-// 	if (data->pl->dir < (float)M_PI)
-// 		a_y *= -1;
-// 	a_x = fabsf(a_y) / tan(data->pl->dir);
-// 	if (check_x_y_win(&ray->hx, &ray->hy) || data->map[(int)ray->hy / SCALE][(int)ray->hx / SCALE] != '1')
-// 		ray->hx += a_x;
-// 		ray->hy += a_y;
-// 	if (check_x_y_win(&ray->hx, &ray->hy) || data->map[(int)ray->hy / SCALE][(int)ray->hx / SCALE] == '1')
-// 		*wall = 1;
-// 	if (*wall == 0)
-// 		my_mlx_pixel_put(data->mlx, data->ray->hx, data->ray->hy, 0x00FF000);
-// 	// ray->len_ray_h = fabs((data->pl->x - ray->hx) / cos(data->pl->dir));
-// }
-
-// void	vertical_check(t_data	*data, int *wall, t_ray	*ver)
-// {
-// 	float	a_x;
-// 	float	a_y;
-
-// 	if (ray->vx == 0 && ray->vy == 0)
-// 	{
-// 		ray->vx = (int)data->pl->x / SCALE * SCALE;
-// 		ray->vy = (int)data->pl->y + (tan(data->pl->dir) * fabsf(ray->vx - data->pl->x));
-// 	}
-// 	a_x = SCALE;
-// 	if (data->pl->dir > (float)M_PI_2 && data->pl->dir < (float)(M_PI * 1.5))
-// 		a_x *= -1;
-// 	a_y = tan(data->pl->dir) * fabsf(a_x - data->pl->x);
-// 	if (check_x_y_win(&ray->vx, &ray->vy) || data->map[(int)ray->vy / SCALE][(int)ray->vx / SCALE] != '1')
-// 	{
-// 		ray->vx += a_x;
-// 		ray->vy += a_y;
-// 	}
-// 	if (check_x_y_win(&ray->vx, &ray->vy) || data->map[(int)ray->vy / SCALE][(int)ray->vx / SCALE] == '1')
-// 		*wall = 1;
-// 	if (*wall == 0)
-// 		my_mlx_pixel_put(data->mlx, data->ray->vx, data->ray->vy, 0x00FF000);
-	// ray->len_ray_v = fabs((data->pl->x - ray->vx) / cos(data->pl->dir));
-// }
-
-// void	check_len_ray(t_data *data)
-// {
-// 	if (data->ray->len_ray_h < data->ray->len_ray_v)
-// 	{
-// 		data->ray->x_ray = data->ray->hx;
-// 		data->ray->y_ray = data->ray->hy;
-// 		data->ray->len_ray = data->ray->len_ray_h;
-// 	}
-// 	else
-// 	{
-// 		data->ray->x_ray = data->ray->vx;
-// 		data->ray->y_ray = data->ray->vy;
-// 		data->ray->len_ray = data->ray->len_ray_v;
-// 	}
-// 	my_mlx_pixel_put(data->mlx, data->ray->x_ray, data->ray->y_ray, 0x00FF000);
-// }
 
 void	init_ver(t_ray *ver, t_pl pl)
 {
-	ver->x = (int)pl.x / SCALE * SCALE;
-	ver->y = (int)pl.y + (tan(pl.dir) * fabsf(ver->x - pl.x));
+	if (pl.dir < (float)M_PI_2 && pl.dir > (float)(M_PI * 1.5))
+		ver->x = (int)pl.x / SCALE * SCALE + 1;
+	else
+		ver->x = (int)pl.x / SCALE * SCALE - 1;
+	ver->y = (int)pl.y + (tan(pl.dir) * fabsf(ver->x - pl.x)) - 1;
 	ver->s_x = SCALE;
 	if (pl.dir > (float)M_PI_2 && pl.dir < (float)(M_PI * 1.5))
 		ver->s_x *= -1;
 	ver->s_y = tan(pl.dir) * fabsf(ver->s_x - pl.x);
+	ver->len_ray = fabs((pl.x - ver->x) / cos(pl.dir));
 }
 
 void	init_hor(t_ray *hor, t_pl pl)
 {
-	hor->y = (int)pl.y / SCALE * SCALE;
+	if (pl.dir < (float)M_PI)
+		hor->y = (int)pl.y / SCALE * SCALE - 1;
+	else
+		hor->y = (int)pl.y / SCALE * SCALE + 1;
 	hor->x = ((int)pl.x + ((fabsf(pl.y - hor->y))) / tan(pl.dir));
 	hor->s_y = SCALE;
 	if (pl.dir < (float)M_PI)
 		hor->s_y *= -1;
 	hor->s_x = fabsf(hor->s_y) / tan(pl.dir);
+	hor->len_ray = fabs((pl.x - hor->x) / cos(pl.dir));
+}
+
+int	check_wall(t_ray *hv, char **map)
+{
+	int	len_map;
+	int	x;
+	int	y;
+
+	x = hv->x / SCALE;
+	y = hv->y / SCALE;
+	len_map = len_arr(map) - 1;
+	if (y > len_map || y < 0 || x < 0 \
+	|| x > (int)ft_strlen(map[y]))
+	{
+		if (y < 0)
+			hv->y = SCALE;
+		if (y > len_map)
+			hv->y = len_map * SCALE;
+		if (x < 0)
+			hv->x = SCALE;
+		if (x > (int)ft_strlen(map[(int)hv->y / SCALE]))
+			hv->x = (int)ft_strlen(map[(int)hv->y / SCALE]) * SCALE;
+		return (1);
+	}
+	if (map[y][x] == '1')
+		return (1);
+	return (0);
 }
 
 void	draw_player(t_data *data)
@@ -142,19 +99,33 @@ void	draw_player(t_data *data)
 	pr = *data->pl;
 	wall_h = 0;
 	wall_v = 0;
+	init_ver(&ver, *data->pl);
+	init_hor(&hor, *data->pl);
+	wall_v = check_wall(&ver, data->map);
+	wall_h = check_wall(&hor, data->map);
 	my_mlx_pixel_put(data->mlx, pr.x, pr.y, 0x00FF000);
+	if (data->pl->dir == (float)M_PI_2 || data->pl->dir == (float)(M_PI * 1.5))
+		wall_v = 1;
+	if ((data->pl->dir == (float)M_PI || data->pl->dir == 0))
+		wall_h = 1;
 	while (!wall_h || !wall_v)
 	{
-		if ((data->pl->dir != (float)M_PI_2 && data->pl->dir != (float)(M_PI * 1.5)) && !wall_v)
-			vertical_check(data, &wall_v);
-		if ((data->pl->dir != (float)M_PI && data->pl->dir != 0) && !wall_h)
-			horizontal_check(data, &wall_h);
-		if (data->pl->dir == (float)M_PI_2 || data->pl->dir == (float)(M_PI * 1.5))
-			wall_v = 1;
-		if (data->pl->dir == (float)M_PI || data->pl->dir == 0)
-			wall_h = 1;
+		if (!wall_v && ver.len_ray <= hor.len_ray)
+		{
+			ver.x += ver.s_x;
+			ver.y += ver.s_y;
+			wall_v = check_wall(&ver, data->map);
+			ver.len_ray = fabs((data->pl->x - ver.x) / cos(data->pl->dir));
+		}
+		if (!wall_h && ver.len_ray >= hor.len_ray)
+		{
+			hor.x += hor.s_x;
+			hor.y += hor.s_y;
+			wall_h = check_wall(&hor, data->map);
+			hor.len_ray = fabs((data->pl->x - hor.x) / cos(data->pl->dir));
+		}
 	}
-	// check_len_ray(data);
+	check_len_ray(hor, ver, data);
 }
 
 void	draw_map(t_data *data)
