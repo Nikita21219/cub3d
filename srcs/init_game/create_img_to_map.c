3d0c20ccdd8a7949ae33e0112450294e54daa64c
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-06-28 16:27:43 +0300

    ray is done...mb

--- srcs/init_game/create_img_to_map.c
+++ srcs/init_game/create_img_to_map.c
@@ -27,41 +27,59 @@ void	draw_block(t_mlx *mlx, int x, int y, int color)
 
 void	check_len_ray(t_ray hor, t_ray ver, t_data *data)
 {
+	hor.len_ray = sqrt(powf(data->pl->x - fabsf(hor.x), 2.0) + powf(data->pl->y - fabsf(hor.y), 2.0));
+	ver.len_ray = sqrt(powf(data->pl->x - fabsf(ver.x), 2.0) + powf(data->pl->y - fabsf(ver.y), 2.0));
 	if (hor.len_ray < ver.len_ray)
-		my_mlx_pixel_put(data->mlx, hor.x, hor.y, 0x00FF000);
+		my_mlx_pixel_put(data->mlx, hor.x / SCALE * SCALE, hor.y / SCALE * SCALE, 0x00FF000);
 	else
-		my_mlx_pixel_put(data->mlx, ver.x, ver.y, 0x00FF000);
+		my_mlx_pixel_put(data->mlx, ver.x / SCALE * SCALE, ver.y / SCALE * SCALE, 0x00FF000);
 }
 
-void	init_ver(t_ray *ver, t_pl pl)
+t_ray	init_ver(t_pl plr, float angle)
 {
-	if (pl.dir < (float)M_PI_2 && pl.dir > (float)(M_PI * 1.5))
-		ver->x = (int)pl.x / SCALE * SCALE + SCALE;
+	t_ray	ver;
+
+	ver.s_x = SCALE;
+	if (cos(angle) > 0)
+		ver.x = (int)plr.x / SCALE * SCALE + SCALE;
 	else
-		ver->x = (int)pl.x / SCALE * SCALE - 1;
-	ver->y = (int)pl.y + (tanf(pl.dir) * fabsf(ver->x - pl.x));
-	ver->s_x = SCALE;
-	ver->s_y = tanf(pl.dir) * fabsf(ver->s_x);
-	if (pl.dir > (float)M_PI_2 && pl.dir < (float)(M_PI * 1.5))
-		ver->s_x *= -1;
-	ver->len_ray = fabs(sqrt(powf(pl.x - fabsf(ver->x), 2.0) + powf(pl.y - fabsf(ver->y), 2.0)));
+	{
+		ver.x = (int)plr.x / SCALE * SCALE - 1;
+		ver.s_x *= -1;
+	}
+	ver.s_y = SCALE * fabs(tan(angle));
+	if (sin(angle) > 0)
+		ver.s_y *= -1;
+	if (sin(angle) > 0)
+		ver.y = (int)plr.y - fabs(((int)plr.x - ver.x) * tan(angle));
+	else
+		ver.y = (int)plr.y + fabs(((int)plr.x - ver.x) * tan(angle));
+	return (ver);
 }
 
-void	init_hor(t_ray *hor, t_pl pl)
+t_ray	init_hor(t_pl plr, float angle)
 {
-	if (pl.dir < (float)M_PI)
-		hor->y = (int)pl.y / SCALE * SCALE - 1;
+	t_ray	hor;
+
+	hor.s_y = SCALE;
+	if (sin(angle) > 0)
+	{
+		hor.y = (int)plr.y / SCALE * SCALE - 1;
+		hor.s_y *= -1;
+	}
 	else
-		hor->y = (int)pl.y / SCALE * SCALE + SCALE;
-	hor->x = (int)pl.x + (fabsf(pl.y - hor->y)) / tanf(pl.dir);
-	hor->s_y = SCALE;
-	if (pl.dir < (float)M_PI)
-		hor->s_y *= -1;
-	hor->s_x = fabsf(hor->s_y) / tanf(pl.dir);
-	hor->len_ray = fabs(sqrt(powf(pl.x - fabsf(hor->x), 2.0) + powf(pl.y - fabsf(hor->y), 2.0)));
+		hor.y = (int)plr.y / SCALE * SCALE + SCALE;
+	hor.s_x = SCALE / fabs(tan(angle));
+	if (cos(angle) < 0)
+		hor.s_x *= -1;
+	if (cos(angle) < 0)
+		hor.x = (int)plr.x - fabs(((int)plr.y - hor.y) / tan(angle));
+	else
+		hor.x = (int)plr.x + fabs(((int)plr.y - hor.y) / tan(angle));
+	return (hor);
 }
 
-int	check_wall(t_ray *hv, char **map)
+int	check_wall(t_ray *hv, char **map, int *wall)
 {
 	int	len_map;
 	int	x;
@@ -78,65 +96,58 @@ int	check_wall(t_ray *hv, char **map)
 		if (y > len_map)
 			hv->y = len_map * SCALE + SCALE;
 		if (x < 0)
-			hv->x = SCALE;
+			hv->x = SCALE - 1;
 		if (x > (int)ft_strlen(map[(int)hv->y / SCALE]))
 			hv->x = (int)ft_strlen(map[(int)hv->y / SCALE]) * SCALE;
+		*wall = 1;
+		hv->len_ray = INFINITY;
 		return (1);
 	}
 	if (map[y][x] == '1')
+	{
+		*wall = 1;
 		return (1);
+	}
 	return (0);
 }
 
-void	draw_player(t_data *data)
+void	rays(t_data *data, float angle)
 {
-	t_pl	pr;
 	t_ray	ver;
 	t_ray	hor;
 	int		wall_h;
 	int		wall_v;
 
-	pr = *data->pl;
 	wall_h = 0;
 	wall_v = 0;
-	init_ver(&ver, *data->pl);
-	init_hor(&hor, *data->pl);
-	printf("ver y=%d, x=%d\n", (int)data->pl->y / SCALE, (int) data->pl->x / SCALE);
-	printf("ver y=%d, x=%d, len=%f\n", (int)ver.y / SCALE, (int)ver.x / SCALE, ver.len_ray);
-	printf("hor y=%d, x=%d, len=%f\n\n\n", (int)hor.y / SCALE, (int)hor.x / SCALE, hor.len_ray);
-	wall_v = check_wall(&ver, data->map);
-	wall_h = check_wall(&hor, data->map);
-	my_mlx_pixel_put(data->mlx, pr.x, pr.y, 0x00FF000);
-	if (data->pl->dir == (float)M_PI_2 || data->pl->dir == (float)(M_PI * 1.5))
+	if (angle != ((float)M_PI / 2) && angle != ((float)M_PI * 1.5))
+		ver = init_ver(*data->pl, angle);
+	else
+	{
 		wall_v = 1;
-	if ((data->pl->dir == (float)M_PI || data->pl->dir == 0))
+		ver.len_ray = INFINITY;
+	}
+	if (angle != 0 && angle != (float)M_PI)
+		hor = init_hor(*data->pl, angle);
+	else
+	{
 		wall_h = 1;
+		hor.len_ray = INFINITY;
+	}
 	while (!wall_h || !wall_v)
 	{
-		if (!wall_v && ver.len_ray <= hor.len_ray)
-		{
-			ver.x += ver.s_x;
-			ver.y += ver.s_y;
-			wall_v = check_wall(&ver, data->map);
-			ver.len_ray = fabs((data->pl->x - ver.x) / cosf(data->pl->dir));
-		}
-		if (!wall_h && ver.len_ray >= hor.len_ray)
+		if (!wall_h && !check_wall(&hor, data->map, &wall_h))
 		{
 			hor.x += hor.s_x;
 			hor.y += hor.s_y;
-			wall_h = check_wall(&hor, data->map);
-			hor.len_ray = fabs((data->pl->x - hor.x) / cosf(data->pl->dir));
 		}
-		if (hor.len_ray > ver.len_ray && wall_v == 1)
-			wall_h = 1;
-		if (ver.len_ray > hor.len_ray && wall_h == 1)
-			wall_v = 1;
-		printf("ver y=%d, x=%d, len=%f\n", (int)ver.y / SCALE, (int)ver.x / SCALE, ver.len_ray);
-		printf("hor y=%d, x=%d, len=%f\n\n\n", (int)hor.y / SCALE, (int)hor.x / SCALE, hor.len_ray);
-		draw_square(data, ver.x, ver.y);
-		draw_square(data, hor.x, hor.y);
+		if (!wall_v && !check_wall(&ver, data->map, &wall_v))
+		{
+			ver.x += ver.s_x;
+			ver.y += ver.s_y;
+		}
 	}
-	// check_len_ray(hor, ver, data);
+	check_len_ray(hor, ver, data);
 }
 
 void	draw_map(t_data *data)
@@ -159,7 +170,15 @@ void	draw_map(t_data *data)
 		}
 		y++;
 	}
-	draw_player(data);
+	data->pl->start = data->pl->dir - M_PI / 6;
+	data->pl->end = data->pl->dir + M_PI / 6;
+	draw_square(data, data->pl->x, data->pl->y);
+	while (data->pl->start <= data->pl->end)
+	{
+		rays(data, data->pl->start);
+		data->pl->start += (M_PI / 3 / WIN_X);
+	}
+	// rays(data, data->pl->dir);
 	mlx_put_image_to_window(data->mlx->mlx, \
 		data->mlx->win, data->mlx->img, 0, 0);
 	mlx_destroy_image(data->mlx->mlx, data->mlx->img);
