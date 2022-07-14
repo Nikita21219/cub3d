deab58814158e58ac6804b60a074a3e9e2de50b6
Rogelio Rast <rrast@at-d6.msk.21-school.ru>
2022-07-14 15:53:14 +0300

    first sprite

--- srcs/init_game/create_img_to_map.c
+++ srcs/init_game/create_img_to_map.c
@@ -33,24 +33,29 @@ void	draw_map(t_data *data)
 	&data->mlx->bpp, &data->mlx->line_l, &data->mlx->endian);
 	data->pl->start = data->pl->dir + ((FOV / 2) * M_PI / 180);
 	data->pl->end = data->pl->dir - ((FOV / 2) * M_PI / 180);
-	// if (data->sprite)
-		// data->sprite->dir = atan2((int)(data->pl->y / SCALE) - data->sprite->y, (int)(data->pl->x / SCALE) - data->sprite->x);
+	if (data->sprite)
+	{
+		data->sprite->dir = atan2((data->pl->y) - (data->sprite->y * SCALE + SCALE / 2), (data->sprite->x * SCALE + SCALE / 2) - (data->pl->x));
+		while (data->sprite->dir - data->pl->dir > M_PI)
+			data->sprite->dir -= 2 * M_PI;
+		while (data->sprite->dir - data->pl->dir < -M_PI)
+			data->sprite->dir += 2 * M_PI;
+		data->sprite->len = sqrt(pow((data->pl->x / SCALE) - (data->sprite->x), 2) + pow(data->pl->y / SCALE - (data->sprite->y), 2));
+	}
+	// printf("dr = %f\n", data->sprite->dir);
 	while (data->pl->start >= data->pl->end)
 	{
 		rays(data, data->pl->start);
 		data->ray->len_ray *= cos(data->pl->start - data->pl->dir);
 		map3d_draw(*data, pix);
-		// if (data->sprite->dir > data->pl->start - ((FOV * M_PI / 180) / WIN_X) && data->sprite->dir < data->pl->start)
-		// {
-		// 	data->sprite->dx = pix;
-		// 	data->sprite->len = sqrt(pow((data->pl->x / SCALE) - (data->sprite->x), 2) + pow(data->pl->y / SCALE - (data->sprite->y), 2));
-		// }
+		if (data->sprite->dir > data->pl->start - ((FOV * M_PI / 180) / WIN_X) && data->sprite->dir < data->pl->start)
+			data->sprite->dx = pix;
 		data->pl->start -= ((FOV * M_PI / 180) / WIN_X);
 		pix++;
 	}
-	// if (data->sprite && data->sprite->dx != 0)
-	// 	draw_sprites(data);
-	// data->sprite->dx = 0;
+	if (data->sprite && data->sprite->dx != 0)
+		draw_sprites(data);
+	data->sprite->dx = 0;
 	draw_2dmap(*data);
 	mlx_put_image_to_window(data->mlx->mlx, \
 		data->mlx->win, data->mlx->img, 0, 0);
