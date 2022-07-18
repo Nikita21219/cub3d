ec395d1f6bf88f7f9527832187ca8dc5d2e685ad
Nikita21219 <madorskii@mail.ru>
2022-07-18 17:12:10 +0300

    fixed a lot of sprites

--- srcs/init_game/create_img_to_map.c
+++ srcs/init_game/create_img_to_map.c
@@ -28,13 +28,13 @@ void	draw_map(t_data *data)
 	int	pix;
 	t_sprite	*sprite;
 
-	sprite = data->sprite;
 	pix = 0;
 	data->mlx->img = mlx_new_image(data->mlx->mlx, WIN_X, WIN_Y);
 	data->mlx->addr = mlx_get_data_addr(data->mlx->img, \
 	&data->mlx->bpp, &data->mlx->line_l, &data->mlx->endian);
 	data->pl->start = data->pl->dir + ((FOV / 2) * M_PI / 180);
 	data->pl->end = data->pl->dir - ((FOV / 2) * M_PI / 180);
+	sprite = data->sprite;
 	while (sprite)
 	{
 		sprite->dir = atan2((data->pl->y) - (sprite->y * SCALE + SCALE / 2), (sprite->x * SCALE + SCALE / 2) - (data->pl->x));
@@ -45,24 +45,26 @@ void	draw_map(t_data *data)
 		sprite->len = sqrt(pow((data->pl->x / SCALE) - (sprite->x), 2) + pow(data->pl->y / SCALE - (sprite->y), 2));
 		sprite = sprite->next;
 	}
-	sprite = data->sprite;
 	while (data->pl->start >= data->pl->end)
 	{
 		rays(data, data->pl->start);
 		data->ray->len_ray *= cos(data->pl->start - data->pl->dir);
 		map3d_draw(*data, pix);
-		while (sprite && sprite->dir > data->pl->start - ((FOV * M_PI / 180) / WIN_X) && sprite->dir < data->pl->start)
+		sprite = data->sprite;
+		while (sprite)
 		{
-			sprite->dx = pix;
+			if (sprite->dir > data->pl->start - ((FOV * M_PI / 180) / WIN_X) && sprite->dir < data->pl->start)
+				sprite->dx = pix;
 			sprite = sprite->next;
 		}
 		data->pl->start -= ((FOV * M_PI / 180) / WIN_X);
 		pix++;
 	}
 	sprite = data->sprite;
-	while (sprite && sprite->dx != 0)
+	while (sprite)
 	{
-		draw_sprite(data, sprite);
+		if (sprite->dx != 0)
+			draw_sprite(data, sprite);
 		sprite = sprite->next;
 	}
 	sprite = data->sprite;
